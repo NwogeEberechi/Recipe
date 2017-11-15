7b9a268903f1afcbda95d98e43a84ba1649551b9
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-11-15 16:09:20 +0000

    create review model

---
+++ server/models/review.js
@@ -0,0 +1,32 @@
+const Review = sequelize.define('Review', {
+    content: DataTypes.STRING,
+    recipeId: {
+      type: DataTypes.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Recipes',
+        key: 'id',
+        as: 'recipeId',
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'userId',
+      }
+    }
+  });
+
+  Review.associate = (models) => {
+    Review.belongsTo(models.Recipe, {
+      foreignKey: 'recipeId',
+    });
+    Review.belongsTo(models.User, {
+      foreignKey: 'userId',
+    });
+  };
+  return Review;
+};
