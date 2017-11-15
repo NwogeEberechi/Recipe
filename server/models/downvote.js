41f9c674102036989645a1d8b9a41b66b6a57b69
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-11-15 16:08:13 +0000

    create model for downvote

---
+++ server/models/downvote.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const Downvote = sequelize.define('Downvote', {
+    recipeId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Recipes',
+        key: 'id',
+        as: 'recipeId',
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: 'Users',
+        key: 'id',
+        as: 'userId',
+      }
+    },
+  });
+
+  Downvote.associate = (models) => {
+    Downvote.belongsTo(models.Recipe, {
+      foreignKey: 'recipeId',
+      onDelete: 'CASCADE'
+    });
+    Downvote.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+  };
+  return Downvote;
+};
