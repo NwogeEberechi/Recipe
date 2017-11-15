4e84f1ef03337c9f375ee7c62f6c178f1f41023d
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-11-15 16:10:05 +0000

    create model for user favorite recipes

---
+++ server/models/favorite.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const Favorite = sequelize.define('Favorite', {
+    recipeId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Recipes',
+        key: 'id',
+        as: 'recipeId',
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: 'Users',
+        key: 'id',
+        as: 'userId',
+      }
+    },
+  });
+
+  Favorite.associate = (models) => {
+    Favorite.belongsTo(models.Recipe, {
+      foreignKey: 'recipeId',
+      onDelete: 'CASCADE'
+    });
+    Favorite.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+  };
+  return Favorite;
+};
