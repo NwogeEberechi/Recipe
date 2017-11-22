c8fe25da7abaaf29ac0db7c41cc19f3d5462dec1
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-11-22 16:32:26 +0000

    add route to add recipe

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+const recipesController = require('../controllers').recipes;
+const usersController = require('../controllers').users;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the recipes API!',
+  }));
+
+  app.post('/api/recipes', recipesController.create);
+  app.post('/api/users/signup', usersController.signup);
+};
