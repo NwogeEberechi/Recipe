be2b85c50134517ab84e57c8f1aa930a4c0691e4
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-11-23 16:02:18 +0000

    add route for posting recipe reviews

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const recipesController = require('../controllers').recipes;
 const usersController = require('../controllers').users;
+const reviewsController = require('../controllers').reviews;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -10,5 +11,6 @@ module.exports = (app) => {
   app.post('/api/recipes', recipesController.create);
   app.get('/api/recipes', recipesController.list);
   app.put('/api/recipes/:recipeId', recipesController.update);
+  app.post('/api/recipes/:recipeId/reviews', reviewsController.postReview);
   app.delete('/api/recipes/:recipeId', recipesController.destroy);
 };
