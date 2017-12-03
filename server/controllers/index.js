42093c7697976119c71252431c73c24c628d021d
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-12-03 23:14:51 +0000

    replace votes with voterecipe

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,7 @@
 const recipes = require('./recipes');
 const users = require('./users');
 const reviews = require('./reviews');
-const votes = require('./votes');
+const votes = require('./voterecipe');
 
 module.exports = {
   recipes,
