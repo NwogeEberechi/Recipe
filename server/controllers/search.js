e628937cdf617ea82a3907a670466e8b2abaef1d
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2017-12-07 12:42:45 +0000

    add method to search recipes using both the user and recipe

---
+++ server/controllers/search.js
@@ -0,0 +1,93 @@
+const Recipe = require('../models').Recipe;
+const User = require('../models').User;
+
+module.exports = {
+	searchByIngredients(req, res) {
+    	const ingredients = req.query.ingredients.split(' ');
+
+    	const query = ingredients.map(item => ({
+      		ingredients: { $iLike: `%${item}%` } }));
+
+    Recipe
+      .findAll({
+        where: {
+          $or: query
+        },
+        include: [
+          { model: User, attributes: ['name', 'updatedAt'] }
+        ]
+      })
+      .then((result) => {
+        if (result.length !==0) {
+          return res.status(200).json({
+            success: true,
+            message: 'Recipes Found',
+            recipe: result
+          });
+        }
+
+        return res.status(404).json({
+          success: true,
+          message: 'No Recipe found',
+        });
+      })
+      .catch(() => res.status(500).json({
+        success: false,
+        message: 'Unable to search recipes' }));
+  },
+
+  searchAll(req, res) {
+  	const searchString = req.query.search;
+  	let foundRecipes;
+
+  	//search recipe
+  	Recipe.findAll({
+  		where: {
+  			$or: [
+  				{ name: {$iLike: `%${searchString}%`} },
+  				{ ingredients: {$iLike: `%${searchString}%`} }
+  			]
+  		},
+  		include: [
+  			{ model: User, attributes: ['name', 'updatedAt'] }
+  		]
+  	})
+  	.then(recipe => {
+  		foundRecipes = recipe;
+  	})
+  	.then(() => {
+  		User.findAll({
+  			attributes: ['name'],
+  			where: {
+  				$or: [
+  					{ name: {$ilike: `%${searchString}%`} },
+  					{ username: {$ilike: `%${searchString}%`} },
+  					{ email: {$ilike: `%${searchString}%`} }
+  				]
+  			},
+  			include: [
+  				{ model: Recipe}
+  			] 
+  		})
+  		.then(user => {
+  			recipes = foundRecipes.concat(user);
+  			if (recipes.length !==0) {
+	  			return res.status(200).json({
+	  			success: true,
+	  			message: `Recipes found`,
+	  			recipes
+	  			});
+  			}
+
+  			return res.status(404).json({
+  				success: true,
+  				message: `No recipes found`
+  			});
+  		});
+  	})
+  	.catch(error => res.status(501).json({
+  		success: false,
+  		message: `unable to search`
+  	}));
+  },
+};
